218d4fea56c22c2677c43c803ad50b5b5aeec6ea
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-01-28 15:09:09 -0500

    [+] Sass: Base Partials - Estilos Generales

--- src/components/journal/JournalPage.js
+++ src/components/journal/JournalPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const JournalPage = () => {
     return (
         <div>
-            <h1>Journal Page</h1>
+            <h1 className="mt-4">Journal Page</h1>
         </div>
     )
 }
