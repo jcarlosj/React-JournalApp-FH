2f20e51e5b0df7f4a5b277f29c85f9c183b756d9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-01-30 11:17:41 -0500

    [+] Sass: Estilos nuevo componente [+] PageComponent: Estructura del formulario

--- src/components/notes/NotePage.js
+++ src/components/notes/NotePage.js
@@ -9,6 +9,29 @@ export const NotePage = () => {
             
             <NotesAppBar />
 
+            <div className="notes__content">
+                
+                <input 
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                >
+                </textarea>
+
+                <div className="notes__image">
+                    <img 
+                        src="https://www.wallpaperup.com/uploads/wallpapers/2014/01/02/213479/ab93c789377a9eaed40518561d7e1d17-500.jpg"
+                        alt="German Shepherd"
+                    />
+                </div>
+
+            </div>
+
         </div>
     )
 }
