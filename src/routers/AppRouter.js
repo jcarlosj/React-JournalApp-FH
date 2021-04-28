e1389cc1bd4f734f4b7e2df6ab2691fab467ae8b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-04-28 11:15:53 -0500

    [+] Routers: Persistencia de datos para mantener estado de autenticacion al recargar

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import {
     Redirect,
     BrowserRouter as Router,
@@ -12,7 +13,29 @@ import { AuthRouter } from './AuthRouter';
 /** Page Components */
 import { JournalPage } from '../components/journal/JournalPage';
 
+/** Configuracion de Firebase */
+import { firebase } from '../firebase/config';
+
+/** Acciones */
+import { login } from '../actions/auth';
+
+/** Component */
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();   /** Despachador de Acciones de Redux */
+
+    useEffect( () => {
+        //  Crea & retorna un Observable (objeto de tipo especial que se puede disparar mas de una vez ante los cambios en el mismo)
+        firebase .auth() .onAuthStateChanged( user => {
+
+            if( user ?.uid ) {
+                console .log( user );
+                dispatch( login( user .uid, user .displayName ) );
+            }
+            
+        });     
+    }, [ dispatch ] );     //  [] => Solo se ejecutara una vez
+
     return (
         <Router>
             <div>
